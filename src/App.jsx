7122f8c0ae4324bc28e27dfde17ec79651cfa406
mrd <dinmuhammadyagafarov@gmail.com>
2023-10-10 12:49:30 +0500

    project upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ButtonM from './components/ButtonM/ButtonM';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonM>Salom</ButtonM>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ButtonM/ButtonM.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './ButtonM.css'
+const ButtonM = ({children}) => {
+  return (
+    <button className='btn' >{children}</button>
+  )
+}
+
+export default ButtonM
